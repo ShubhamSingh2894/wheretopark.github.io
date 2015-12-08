783d9733123898e39cdf1ad22c2f3b1438ba3b8b
Sagar Chand Agarwal <atomsagar@gmail.com>
2015-12-09 00:31:31 +0530

    revamp

--- js/hits.js
+++ js/hits.js
@@ -1,8 +1,8 @@
 $(function() {
 	Parse.$ = jQuery;
 	Parse.initialize("ORdRb6ZtoTRSIUBeDKCcpigKQR9Pr6YJjeguuN9t", "ec8NHsZDP17pTdRjBKQFAPxB8ihduGt6e8NAGdaH");
-	var TestObject1 = Parse.Object.extend("TestObject1");
-    var testObject1 = new TestObject1();
-    testObject1.save({hits: "WhereToPark"}).then(function(object) {
+	var visits_9_12 = Parse.Object.extend("visits_9_12");
+    var visits = new visits_9_12();
+    visits.save({hits: "WhereToPark"}).then(function(object) {
     });
 });
