3ac85f7ca15369b586f9054f68c01c538a5fa778
Sagar Chand Agarwal <atomsagar@gmail.com>
2015-12-08 07:14:30 +0530

    hits reconfigured

--- js/hits.js
+++ js/hits.js
@@ -1,9 +1,6 @@
 $(function() {
- 
-    Parse.$ = jQuery;
- 
-    // Replace this line with the one on your Quickstart Guide Page
-	Parse.initialize("SxLcanZTIZ5uJxDqtE65d0NPxk1zYRxZSxt6E5oq", "JdEN6kYT4thC4AzBS4nMfAavGDszgP24biD4KRmy"); 
+	Parse.$ = jQuery;
+	Parse.initialize("ORdRb6ZtoTRSIUBeDKCcpigKQR9Pr6YJjeguuN9t", "ec8NHsZDP17pTdRjBKQFAPxB8ihduGt6e8NAGdaH");
 	var TestObject1 = Parse.Object.extend("TestObject1");
     var testObject1 = new TestObject1();
     testObject1.save({hits: "WhereToPark"}).then(function(object) {
