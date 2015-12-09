62a0714a93e418cb2bca1247c027628641af5b59
Sagar Chand Agarwal <atomsagar@gmail.com>
2015-12-09 23:04:09 +0000

    hits reconfigured

--- js/hits.js
+++ js/hits.js
@@ -1,8 +1,8 @@
 $(function() {
 	Parse.$ = jQuery;
 	Parse.initialize("ORdRb6ZtoTRSIUBeDKCcpigKQR9Pr6YJjeguuN9t", "ec8NHsZDP17pTdRjBKQFAPxB8ihduGt6e8NAGdaH");
-	var visits_9_12 = Parse.Object.extend("visits_9_12");
-    var visits = new visits_9_12();
+	var visits_10_12 = Parse.Object.extend("visits_10_12");
+    var visits = new visits_10_12();
     visits.save({hits: "WhereToPark"}).then(function(object) {
     });
 });
