911d33421f7a7eda853deddc202e3eabc0494360
Sagar Chand Agarwal <atomsagar@gmail.com>
2015-12-14 07:48:48 +0530

    timings extended

--- js/hits.js
+++ js/hits.js
@@ -1,8 +1,8 @@
 $(function() {
 	Parse.$ = jQuery;
 	Parse.initialize("ORdRb6ZtoTRSIUBeDKCcpigKQR9Pr6YJjeguuN9t", "ec8NHsZDP17pTdRjBKQFAPxB8ihduGt6e8NAGdaH");
-	var visits_11_12 = Parse.Object.extend("visits_11_12");
-    var visits = new visits_11_12();
+	var visits_14_12 = Parse.Object.extend("visits_14_12");
+    var visits = new visits_14_12();
     visits.save({hits: "WhereToPark"}).then(function(object) {
     });
 });
